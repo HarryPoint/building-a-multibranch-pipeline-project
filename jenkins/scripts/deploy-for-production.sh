524658647d3b0ee1693a92cf8cac2a51caee0b8f
HarryPoint <1751826104@qq.com>
2019-11-12 13:51:11 +0800

    xiguai

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -37,3 +37,7 @@ echo 'Now...'
 echo 'Visit http://localhost:5000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 5000:5000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
+set -x
+sleep 30
+set +x
+echo 'end------'
